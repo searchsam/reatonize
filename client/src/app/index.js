828834932b33f57632fe3faa7b53566eaae67380
Samuel José Gutiérrez Avilés <samaviles@gmail.com>
2020-10-09 02:09:53 -0600

    Tipografia

--- client/src/app/index.js
+++ client/src/app/index.js
@@ -5,22 +5,26 @@ import "./index.css";
 const lista = require("./lista.json");
 
 let left = document.querySelector("ul.left-list");
+let center = document.querySelector("ul.center-list");
 let rigth = document.querySelector("ul.rigth-list");
 let list = document.getElementById("lista");
 let canto = document.getElementById("canto");
 let state;
 
-let selector = true;
+let selector = 0;
 lista.forEach((element, index) => {
   let li = document.createElement('li');
   li.innerHTML = `<a href="${index}" class="canto">${element.nombre}</a>`;
 
-  if (selector) {
+  if (selector == 0) {
     left.appendChild(li);
-    selector = false;
-  } else {
+    selector = 1;
+  } else if (selector == 1) {
+    center.appendChild(li);
+    selector = 2;
+  } else if (selector == 2) {
     rigth.appendChild(li);
-    selector = true;
+    selector = 0;
   }
 });
 
